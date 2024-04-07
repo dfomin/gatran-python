cb2d9d0de49631a4252c09556cbca7b7a02295e8
Dmitry Fomin <fomin.dm@gmail.com>
2024-04-07 21:57:38 +0200

    Compilation fix

--- examples/tictactoe/tictactoe_action.py
+++ examples/tictactoe/tictactoe_action.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from typing import List, Tuple
 
-from examples.tictactoe.tictactoe_state import TicTacToeState
 from gatran.action import Action
 
 
@@ -12,7 +11,7 @@ class TicTacToeAction(Action):
     def __str__(self) -> str:
         return f"pos={self.pos}"
 
-    def apply(self, state: TicTacToeState) -> List[Tuple[TicTacToeState, float]]:
+    def apply(self, state: 'TicTacToeState') -> List[Tuple['TicTacToeState', float]]:
         state.field |= 1 << (self.pos * 2 + state.current_player_index)
         state.check_game_over()
         if not state.is_finished:
